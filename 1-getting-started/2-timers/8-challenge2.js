40941088e9577291cc0b9c8caa273c6bf062de06
ancc02 <oandresccc@gmail.com>
2022-10-07 14:11:29 -0500

    lessons two and three

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -1,5 +1,9 @@
-// Print "Hello World"
-// Every second
-// And stop after 5 times
-
-// After 5 times. Print "Done" and let Node exit.
+let counter = 0;
+const interval = setInterval(() => {
+    console.log('hello every 1 second');
+    counter++;
+    if (counter > 4) {
+        console.log('done');
+        clearInterval(interval);
+    }
+}, 1000);
